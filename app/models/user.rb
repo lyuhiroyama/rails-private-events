4f505c82bb6f42b3c3805381aab57518702a1360
lyuhiroyama <lyuhiroyama@gmail.com>
2025-07-29 14:46:03 +0900

    feat: set up Devise authentication & integrate Devise into User model
    - Ran migration afterwards also.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :registrations
   has_many :attending_events, through: :registrations, source: :event
   has_many :events_created, class_name: "Event", foreign_key: :creator_id
