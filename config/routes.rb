048e737cee375e1a02aad358052f9f6f7afa818d
lyuhiroyama <lyuhiroyama@gmail.com>
2025-07-29 14:22:11 +0900

    feat: set root route to events#index
    Also edited index.html.erb header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "events#index"
+  
   resources :registrations
   resources :events
   resources :users
@@ -11,7 +13,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
