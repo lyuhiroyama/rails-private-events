5c057e51b6bb79aa9b73f72a983bf9cf13ec43a8
lyuhiroyama <lyuhiroyama@gmail.com>
2025-08-01 02:00:20 +0900

    feat: separate events#index page events into upcoming & past events
    - Add self.past & self.upcoming class methods to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   has_many :event_registrations
   has_many :users_attending, through: :event_registrations, source: :user
   belongs_to :event_creator, class_name: "User", foreign_key: :creator_id
+
+  def self.past
+    where("event_date < ?", Date.today)
+  end
+
+  def self.upcoming
+    where("event_date > ?", Date.today)
+  end
 end
