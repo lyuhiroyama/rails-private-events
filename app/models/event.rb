98ba5a77f0471d475f45f5ce9b01c043b6c0a457
lyuhiroyama <lyuhiroyama@gmail.com>
2025-08-01 18:50:23 +0900

    feat: define associations between the models: Event, User, and Invitation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  has_many :invitations
+  has_many :invited_users, through: :invitations, source: :user
   has_many :event_registrations
   has_many :users_attending, through: :event_registrations, source: :user
   belongs_to :event_creator, class_name: "User", foreign_key: :creator_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :invitations
+  has_many :invited_events, through: :invitations, source: :event
   has_many :event_registrations
   has_many :attending_events, through: :event_registrations, source: :event
   has_many :events_created, class_name: "Event", foreign_key: :creator_id
